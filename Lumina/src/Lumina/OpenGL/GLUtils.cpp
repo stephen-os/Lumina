dc34ce26da99b4e668a81048f1bf2231642601d5
Reset <imstephen.1sw@gmail.com>
2024-08-06 21:37:29 -0400

    GLUtils logs error enum.

--- Lumina/src/Lumina/OpenGL/GLUtils.cpp
+++ Lumina/src/Lumina/OpenGL/GLUtils.cpp
@@ -5,17 +5,33 @@
  #include <glad/glad.h>
 #include <iostream>
 
+std::string GetGLErrorString(GLenum error)
+{
+    switch (error)
+    {
+    case GL_INVALID_ENUM:                  return "GL_INVALID_ENUM";
+    case GL_INVALID_VALUE:                 return "GL_INVALID_VALUE";
+    case GL_INVALID_OPERATION:             return "GL_INVALID_OPERATION";
+    case GL_STACK_OVERFLOW:                return "GL_STACK_OVERFLOW";
+    case GL_STACK_UNDERFLOW:               return "GL_STACK_UNDERFLOW";
+    case GL_OUT_OF_MEMORY:                 return "GL_OUT_OF_MEMORY";
+    case GL_INVALID_FRAMEBUFFER_OPERATION: return "GL_INVALID_FRAMEBUFFER_OPERATION";
+    default:                               return "UNKNOWN_ERROR";
+    }
+}
+
 void GLClearError()
 {
-	while (glGetError() != GL_NO_ERROR);
+    while (glGetError() != GL_NO_ERROR);
 }
 
 bool GLLogCall(const char* function, const char* file, int line)
 {
-	while (GLenum error = glGetError())
-	{
-		std::cout << "[OpenGL Error] (" << error << "): " << function << " " << file << ":" << line << std::endl;
-		return false;
-	}
-	return true;
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL Error] (" << error << "): " << GetGLErrorString(error)
+            << " - " << function << " " << file << ":" << line << std::endl;
+        return false;
+    }
+    return true;
 }
