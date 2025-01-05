611033007e44e1f31921fd711d83c179cec61c69
Reset <imstephen.1sw@gmail.com>
2025-01-05 03:30:33 -0500

    Changed BufferInfo to a layout and element structure.

--- Lumina/src/Lumina/Renderer/Buffer.h
+++ Lumina/src/Lumina/Renderer/Buffer.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <string>
+#include <vector>
+
 #include <glad/glad.h>
 
 namespace Lumina
@@ -30,13 +32,75 @@ namespace Lumina
 		return 0;
 	}
 
-	struct BufferInfo
+	struct BufferElement
 	{
 		std::string Name;
 		BufferDataType Type;
-		uint32_t Count;
-		uint32_t Stride;
 		uint32_t Size;
+		size_t Offset;
+		bool Normalized;
+
+		BufferElement() = default;
+
+		BufferElement(BufferDataType type, const std::string& name, bool normalized = false)
+			: Name(name), Type(type), Size(CalculateDataTypeSize(type)), Offset(0), Normalized(normalized)
+		{
+		}
+
+		uint32_t GetComponentCount() const
+		{
+			switch (Type)
+			{
+				case BufferDataType::Float:   return 1;
+				case BufferDataType::Float2:  return 2;
+				case BufferDataType::Float3:  return 3;
+				case BufferDataType::Float4:  return 4;
+				case BufferDataType::Mat3:    return 3;
+				case BufferDataType::Mat4:    return 4;
+				case BufferDataType::Int:     return 1;
+				case BufferDataType::Int2:    return 2;
+				case BufferDataType::Int3:    return 3;
+				case BufferDataType::Int4:    return 4;
+				case BufferDataType::Bool:    return 1;
+			}
+
+			return 0;
+		}
+	};
+
+	class BufferLayout
+	{
+	public:
+		BufferLayout() = default;
+
+		BufferLayout(std::initializer_list<BufferElement> elements)
+			: m_Elements(elements)
+		{
+			CalculateStride();
+		}
+
+		uint32_t GetStride() const { return m_Stride; }
+		const std::vector<BufferElement>& GetElements() const { return m_Elements; }
+
+		std::vector<BufferElement>::iterator begin() { return m_Elements.begin(); }
+		std::vector<BufferElement>::iterator end() { return m_Elements.end(); }
+		std::vector<BufferElement>::const_iterator begin() const { return m_Elements.begin(); }
+		std::vector<BufferElement>::const_iterator end() const { return m_Elements.end(); }
+	private:
+		void CalculateStride()
+		{
+			size_t offset = 0;
+			m_Stride = 0;
+			for (auto& element : m_Elements)
+			{
+				element.Offset = offset;
+				offset += element.Size;
+				m_Stride += element.Size;
+			}
+		}
+	private:
+		std::vector<BufferElement> m_Elements;
+		uint32_t m_Stride = 0;
 	};
 
 	class VertexBuffer
@@ -53,11 +117,11 @@ namespace Lumina
 
 		void SetData(const void* data, uint32_t size);
 
-		const BufferInfo& GetInfo() const { return m_BufferInfo; }
-		void SetInfo(const BufferInfo& info) { m_BufferInfo = info; }
+		const BufferLayout& GetInfo() const { return m_BufferLayout; }
+		void SetLayout(const BufferLayout& layout) { m_BufferLayout = layout; }
 	private:
 		uint32_t m_BufferID;
-		BufferInfo m_BufferInfo;
+		BufferLayout m_BufferLayout;
 	};
 
 	class IndexBuffer
