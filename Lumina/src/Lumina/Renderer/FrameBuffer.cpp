a38919172c3c12ebfa05e0a94d14cbebfa40fae5
Reset <imstephen.1sw@gmail.com>
2025-01-11 04:46:07 -0500

    Changed frame buffer color attachment from GL_LINEAR to GL_NEAREST.

--- Lumina/src/Lumina/Renderer/FrameBuffer.cpp
+++ Lumina/src/Lumina/Renderer/FrameBuffer.cpp
@@ -18,10 +18,11 @@ namespace Lumina
         GLCALL(glGenTextures(1, &m_ColorAttachment));
         GLCALL(glBindTexture(GL_TEXTURE_2D, m_ColorAttachment));
         GLCALL(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_Width, m_Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr));
-        GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
-        GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+        GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
+        GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
         GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
         GLCALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
+        GLCALL(glGenerateMipmap(GL_TEXTURE_2D));
 
         GLCALL(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_ColorAttachment, 0));
 
@@ -69,6 +70,7 @@ namespace Lumina
         // Resize the color attachment (texture)
         GLCALL(glBindTexture(GL_TEXTURE_2D, m_ColorAttachment));
         GLCALL(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr));
+        GLCALL(glGenerateMipmap(GL_TEXTURE_2D));
 
         // Resize the depth attachment (renderbuffer)
         GLCALL(glBindRenderbuffer(GL_RENDERBUFFER, m_DepthAttachment));
