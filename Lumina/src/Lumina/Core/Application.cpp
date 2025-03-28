49a8ced697225b863d974d5f906f0670967227ed
Resetss <imstephen.1sw@gmail.com>
2025-03-27 22:45:14 -0400

    Removed TestVulkan()

--- Lumina/src/Lumina/Core/Application.cpp
+++ Lumina/src/Lumina/Core/Application.cpp
@@ -24,36 +24,8 @@ namespace Lumina
         std::cerr << description << ".\n";
     }
 
-    static void TestVulkan()
-    {
-        VkInstance instance;
-
-        VkApplicationInfo appInfo{};
-        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-        appInfo.pApplicationName = "Lumina Vulkan Test";
-        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.pEngineName = "No Engine";
-        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.apiVersion = VK_API_VERSION_1_0;
-
-        VkInstanceCreateInfo createInfo{};
-        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-        createInfo.pApplicationInfo = &appInfo;
-
-        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
-            std::cerr << "[VULKAN ERROR] Failed to create Vulkan instance!\n";
-        }
-        else {
-            std::cout << "[VULKAN SUCCESS] Vulkan instance created successfully.\n";
-        }
-
-        vkDestroyInstance(instance, nullptr);
-    }
-
     Application::Application(const ApplicationSpecification& applicationSpecification)
     {
-        // TestVulkan(); 
-
         m_Specifications = applicationSpecification;
 
         glfwSetErrorCallback(GLFWErrorCallback);
