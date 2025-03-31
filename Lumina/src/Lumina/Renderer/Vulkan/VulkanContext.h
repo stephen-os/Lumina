5b9bbdf89702647c89a5eb6aae9894bf52dee761
Resetss <imstephen.1sw@gmail.com>
2025-03-31 19:00:16 -0400

    Broke apart context initilization.

--- Lumina/src/Lumina/Renderer/Vulkan/VulkanContext.h
+++ Lumina/src/Lumina/Renderer/Vulkan/VulkanContext.h
@@ -49,24 +49,34 @@ namespace Lumina
 		void PostRender() override;
 		void Shutdown() override;
 	private:
+		void CreateInstance();
+		void SelectGPU(); 
+		void SelectQueueFamily();
+		void CreateLogicalDevice();
+		void CreateDescriptorPools();
+		void CreateFramebuffer();
+		void CreateContext();
+		void UploadFont(); 
+	private:
 		GLFWwindow* m_Window = nullptr;
 
-		VkAllocationCallbacks* m_Allocator = NULL;
-		VkInstance               m_Instance = VK_NULL_HANDLE;
-		VkPhysicalDevice         m_PhysicalDevice = VK_NULL_HANDLE;
-		VkDevice                 m_Device = VK_NULL_HANDLE;
-		uint32_t                 m_QueueFamily = (uint32_t)-1;
-		VkQueue                  m_Queue = VK_NULL_HANDLE;
-		VkDebugReportCallbackEXT m_DebugReport = VK_NULL_HANDLE;
-		VkPipelineCache          m_PipelineCache = VK_NULL_HANDLE;
-		VkDescriptorPool         m_DescriptorPool = VK_NULL_HANDLE;
+		VkAllocationCallbacks*		m_Allocator = NULL;
+		VkInstance					m_Instance = VK_NULL_HANDLE;
+		VkPhysicalDevice			m_PhysicalDevice = VK_NULL_HANDLE;
+		VkDevice					m_Device = VK_NULL_HANDLE;
+		uint32_t					m_QueueFamily = (uint32_t)-1;
+		VkQueue						m_Queue = VK_NULL_HANDLE;
+		VkDebugReportCallbackEXT	m_DebugReport = VK_NULL_HANDLE;
+		VkPipelineCache				m_PipelineCache = VK_NULL_HANDLE;
+		VkDescriptorPool			m_DescriptorPool = VK_NULL_HANDLE;
+		VkSurfaceKHR				m_Surface = VK_NULL_HANDLE;
 
-		ImGui_ImplVulkanH_Window m_MainWindowData;
-		int                      m_MinImageCount = 2;
-		bool                     m_SwapChainRebuild = false;
+		ImGui_ImplVulkanH_Window	m_MainWindowData;
+		int							m_MinImageCount = 2;
+		bool						m_SwapChainRebuild = false;
 
 		// Per-frame-in-flight
-		std::vector<std::vector<VkCommandBuffer>> m_AllocatedCommandBuffers;
+		std::vector<std::vector<VkCommandBuffer>>		m_AllocatedCommandBuffers;
 		std::vector<std::vector<std::function<void()>>> m_ResourceFreeQueue;
 
 		// Unlike g_MainWindowData.FrameIndex, this is not the the swapchain image index
