1632d337b7a9df736f1f0b4003f3afb97ac43289
stephen-os <ImStephen.1sw@gmail.com>
2025-05-07 21:38:52 -0400

    Added Aliase into namespace Lumina

--- Lumina/src/Lumina/Core/Aliases.h
+++ Lumina/src/Lumina/Core/Aliases.h
@@ -2,20 +2,23 @@
 
 #include <memory>
 
-template<typename T>
-using Shared = std::shared_ptr<T>;
-
-template <typename T, typename... Args>
-constexpr Shared<T> MakeShared(Args&&... args)
+namespace Lumina
 {
-    return std::make_shared<T>(std::forward<Args>(args)...);
-}
+    template<typename T>
+    using Shared = std::shared_ptr<T>;
 
-template<typename T>
-using Unique = std::unique_ptr<T>;
+    template <typename T, typename... Args>
+    constexpr Shared<T> MakeShared(Args&&... args)
+    {
+        return std::make_shared<T>(std::forward<Args>(args)...);
+    }
 
-template <typename T, typename... Args>
-constexpr Unique<T> MakeUnique(Args&&... args)
-{
-    return std::make_unique<T>(std::forward<Args>(args)...);
+    template<typename T>
+    using Unique = std::unique_ptr<T>;
+
+    template <typename T, typename... Args>
+    constexpr Unique<T> MakeUnique(Args&&... args)
+    {
+        return std::make_unique<T>(std::forward<Args>(args)...);
+    }
 }
