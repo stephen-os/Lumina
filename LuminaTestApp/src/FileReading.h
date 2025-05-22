6bebfdbcc4a90ccc1dff940d2117a4d82796c5be
resetss <imstephen.1sw@gmail.com>
2025-05-22 16:42:01 -0400

    Implemented File utilities and added test files to reflect File IO usage.

--- LuminaTestApp/src/FileReading.h
+++ LuminaTestApp/src/FileReading.h
@@ -1,47 +1,75 @@
-#include <vector>
-#include <iostream>
-#include <string>
+#pragma once
 
-#include "imgui.h"
+#include <vector>
+#include <string>
+#include <cctype>  // for isprint
+#include <imgui.h>
 
 #include "Lumina/Core/Layer.h"
-#include "Lumina/Core/Aliases.h"
 #include "Lumina/Core/Log.h"
-#include "Lumina/Utils/Timer.h"
-#include "Lumina/Renderer/Renderer.h"
-#include "Lumina/Renderer/TextureAtlas.h"
+#include "Lumina/IO/File.h"
 
-#include "Lumina/Renderer/Cameras/OrthographicCamera.h"
-#include "Lumina/Renderer/Cameras/PerspectiveCamera.h"
-
-#include "Lumina/Utils/FileReader.h"
-#include <algorithm>
-
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-
-// Dev Dependencies
 #include "Lumina/Core/Input.h"
 #include "Lumina/Core/KeyCode.h"
 
 class FileReading : public Lumina::Layer
 {
 public:
-    virtual void OnAttach() override
-    {
-
-    }
+    virtual void OnAttach() override {}
 
     virtual void OnDetach() override {}
 
-    virtual void OnUpdate(float ts) override
-    {
-    }
+    virtual void OnUpdate(float ts) override {}
 
     virtual void OnUIRender() override
     {
-        ImGui::Begin("Test Layer");
-		ImGui::End(); 
+        static std::string textFileContent;
+        static std::string binaryFileContent;
+        static bool loaded = false;
+
+        if (!loaded)
+        {
+            if (Lumina::File::Exists("res/files/textfile.txt"))
+                textFileContent = Lumina::File::ReadText("res/files/textfile.txt");
+            else
+                textFileContent = "[textfile.txt not found]";
+
+            if (Lumina::File::Exists("res/files/binaryfile.bin"))
+                binaryFileContent = Lumina::File::ReadBinary("res/files/binaryfile.bin");
+            else
+                binaryFileContent = "[binaryfile.bin not found]";
+
+            loaded = true;
+        }
+
+        ImGui::Begin("File I/O Test");
+
+        ImGui::Text("Text File Content:");
+        ImGui::InputTextMultiline("##TextFile", textFileContent.data(), textFileContent.size() + 1, ImVec2(400, 100), ImGuiInputTextFlags_ReadOnly);
+
+        ImGui::Separator();
+
+        ImGui::Text("Binary File Content (as characters):");
+        std::string printableBinary;
+        for (char c : binaryFileContent)
+            printableBinary += (std::isprint(static_cast<unsigned char>(c)) ? c : '.');
+
+        ImGui::InputTextMultiline("##BinaryFile", printableBinary.data(), printableBinary.size() + 1, ImVec2(400, 100), ImGuiInputTextFlags_ReadOnly);
+
+        ImGui::Separator();
+
+        if (ImGui::Button("Save Alternate Text File"))
+        {
+            std::string altText = "This is an alternate text file saved from ImGui.";
+            Lumina::File::WriteText("res/files/alt_textfile.txt", altText);
+        }
+
+        if (ImGui::Button("Save Alternate Binary File"))
+        {
+            std::string altBinary = "\x42\x69\x6E\x61\x72\x79\x21"; // "Binary!"
+            Lumina::File::WriteBinary("res/files/alt_binaryfile.bin", altBinary);
+        }
+
+        ImGui::End();
     }
-private:
 };
